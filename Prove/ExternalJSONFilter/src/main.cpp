0dbc7bcfa41556bb787aed0a970099c09028098f
Davide Busolin <davide.busolin@studenti.unimi.it>
2021-03-31 14:05:39 +0200

    Aggiunta funzione sia HTTP che HTTPS

--- Prove/ExternalJSONFilter/src/main.cpp
+++ Prove/ExternalJSONFilter/src/main.cpp
@@ -12,25 +12,27 @@ const char *password = "IOT_TEST";
 //Possibilmente successivamente impostati "da fuori"
 
 float min_num = 0;
-float max_num = 150;
+float max_num = 80000;
 //Costante PWMRANGE
 
 //Come prova faccio una richiesta a http://www.randomnumberapi.com/api/v1.0/random?min=0&max=100
-//String apiUrl = "http://www.randomnumberapi.com/api/v1.0/random?min=" + String(min_num) + "&max=" + String(max_num);
+//String apiUrl = "http://www.randomnumberapi.com/api/v1.0/random?min=" + String(int(min_num)) + "&max=" + String(int(max_num)); //http_random
 //String apiUrl = "https://api.blockchain.com/v3/exchange/tickers/BTC-USD";
 //String apiUrl = "https://api.ratesapi.io/api/latest";
 //String apiUrl = "https://api.zippopotam.us/us/90210"; //Beverly Hills (scelta perché ha un mix di oggetti e array)
-String apiUrl = "https://csrng.net/csrng/csrng.php?min=" + String(int(min_num)) + "&max=" + String(int(max_num));
+String apiUrl = "https://csrng.net/csrng/csrng.php?min=" + String(int(min_num)) + "&max=" + String(int(max_num)); //https_random
 
 //Provo a creare un filtro a partire da un input esterno
-//String filterJSON = "[true]";
+//String filterJSON = "[true]"; //http_random
 //String filterJSON = "{last_trade_price: true}"; //PER API BLOCKCHAIN
-//String filterJSON = "{\"places\": [{\"latitude\": true}]}";
+//String filterJSON = "{\"places\": [{\"latitude\": true}]}"; //Beverly Hills
 //String filterJSON = "{rates: {MXN: true}}"; //PER API RATES
-String filterJSON = "[{random: true}]";
-//String path = "last_trade_price";
-//String path = "places/0/latitude";
-String path = "0/random";
+String filterJSON = "[{random: true}]"; //https_random
+
+//String path = "0"; //http_random
+//String path = "last_trade_price"; //PER API BLOCKCHAIN
+//String path = "places/0/latitude"; //Beverly Hills
+String path = "0/random"; //https_random
 
 unsigned long last_action = 0;
 int out_pwm;
@@ -59,9 +61,7 @@ void setup_wifi() {
 }
 
 void http_request(void (*callback)(Stream &)) {
-  BearSSL::WiFiClientSecure client;
-  client.setInsecure();
-
+  WiFiClient client;
   HTTPClient http;
 
   Serial.print("[HTTP] begin...\n");
@@ -79,9 +79,8 @@ void http_request(void (*callback)(Stream &)) {
 
       // file found at server
       if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-        //Serial.println(http.getString());
-        //WiFiClient stream = http.getStream();
-        callback(client);
+        WiFiClient stream = http.getStream();
+        callback(stream);
       }
     } else {
       Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
@@ -89,11 +88,44 @@ void http_request(void (*callback)(Stream &)) {
 
     http.end();
   } else {
-    Serial.printf("[HTTP} Unable to connect\n");
+    Serial.printf("[HTTP] Unable to connect\n");
   }
 }
 
-void http_callback(Stream &stream) {
+void https_request(void (*callback)(Stream &)) {
+  BearSSL::WiFiClientSecure client;
+  client.setInsecure();
+
+  HTTPClient http;
+
+  Serial.print("[HTTPS] begin...\n");
+  if (http.begin(client, apiUrl)) { // HTTP
+
+    //Serial.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    //http.addHeader("Content-Type", "application/json");
+    int httpCode = http.GET();
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+        callback(client);
+      }
+    } else {
+      Serial.printf("[HTTPS] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+  } else {
+    Serial.printf("[HTTPS] Unable to connect\n");
+  }
+}
+
+void stream_callback(Stream &stream) {
   LoggingStream ls(stream, Serial);
 
   StaticJsonDocument<200> filter;
@@ -183,7 +215,11 @@ void loop() {
   if (now - last_action > 5000) {
     last_action = now;
     if (WiFi.status() == WL_CONNECTED) {
-      http_request(http_callback);
+      if (apiUrl.startsWith("https://")) {
+        https_request(stream_callback);
+      } else {
+        http_request(stream_callback);
+      }
     }
     analogWrite(LED_PIN, out_pwm);
     Serial.print("Free heap: ");
