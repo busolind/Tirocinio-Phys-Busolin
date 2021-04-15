d0f975908448fa2282fe1185d557650a8d5f3d87
Davide Busolin <davide.busolin@studenti.unimi.it>
2021-04-15 15:10:47 +0200

    Added OTA

--- Prove/Prototipo/src/main.cpp
+++ Prove/Prototipo/src/main.cpp
@@ -2,6 +2,7 @@
 #include "ESPAsyncWebServer.h"
 #include <Arduino.h>
 #include <ArduinoJson.h>
+#include <ArduinoOTA.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncWiFiManager.h>
@@ -133,6 +134,54 @@ void setup_ws() {
     request->send(404, "text/plain", "Not found");
   });
   server.begin();
+}
+
+//OTA setup
+void setup_ota() {
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  //ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  ArduinoOTA.setPassword("prototipo");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+      LittleFS.end();
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("[OTA] Ready");
+  Serial.println(WiFi.localIP());
 }
 
 // WiFi setup
@@ -442,6 +491,7 @@ String conf_to_json() {
 void setup() {
   Serial.begin(115200);
   setup_wifi();
+  setup_ota();
   setup_ws();
   pinMode(OUT_PIN, OUTPUT);
   LittleFS.begin();
@@ -465,6 +515,7 @@ void loop() {
 
   if (WiFi.status() == WL_CONNECTED) {
     request_task.enableIfNot();
+    ArduinoOTA.handle();
   } else {
     request_task.disable();
   }
